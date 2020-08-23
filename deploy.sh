351a767d8bf03ad3f45feb780c00c910a3797209
Martin McDowell <martin.mcdowell@gmail.com>
2020-08-23 19:34:36 +0100

    Test1

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,10 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
-cd public
+# cd public
 
 # Add changes to git.
-git add .
+git add -A
 
 # Commit changes.
 msg="rebuilding site $(date)"
